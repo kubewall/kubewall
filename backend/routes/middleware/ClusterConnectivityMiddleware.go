d8359db02f2433b0503ddeafd5e334d1d29851d6
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2024-10-01 13:35:52 +0530

    chore: adding log lines on failure.

--- backend/routes/middleware/ClusterConnectivityMiddleware.go
+++ backend/routes/middleware/ClusterConnectivityMiddleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	"github.com/charmbracelet/log"
 	"github.com/kubewall/kubewall/backend/container"
 	"github.com/labstack/echo/v4"
 	"net/http"
@@ -24,6 +25,7 @@ func ClusterConnectivityMiddleware(container container.Container) echo.Middlewar
 			if !container.Cache().Has(isAbleToConnectToClusterCacheKey) {
 				_, err = container.DiscoveryClient(config, cluster).ServerVersion()
 				if err != nil {
+					log.Error("failed to connect to cluster", "err", err)
 					container.Cache().Set(isAbleToConnectToClusterCacheKey, false)
 					return c.JSON(http.StatusInternalServerError, err.Error())
 				}
@@ -32,6 +34,7 @@ func ClusterConnectivityMiddleware(container container.Container) echo.Middlewar
 
 			value, _ := container.Cache().Get(isAbleToConnectToClusterCacheKey)
 			if value == false {
+				log.Error("previously failed to connect to this cluster, please read-load config or check network-connection")
 				return c.JSON(http.StatusInternalServerError, "Cluster is not available or failed to connect to cluster, please check network connection")
 			}
 
