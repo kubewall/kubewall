612b8984b83cf100808a577515355da1a101f59b
Abhimanyu Sharma <abhimanyusharma003@gmail.com>
2025-07-30 03:34:27 +0530

    fix: remove unnecessary div wrapper in CurrentByDesiredCell component

--- client/src/components/app/Table/TableCells/currentByDesiredCell.tsx
+++ client/src/components/app/Table/TableCells/currentByDesiredCell.tsx
@@ -9,12 +9,9 @@ const CurrentByDesiredCell = memo(function ({ cellValue }: CurrentByDesiredCellP
   const valueArray = cellValue.split('/');
   const isReady = valueArray[0] === valueArray[1];
   return (
-
-    <div className="">
-      <span className={`text-sm truncate  px-3 ${isReady ? 'text-emerald-600 dark:text-emerald-400' :'text-red-600 dark:text-red-400'}`}>
-        {cellValue}
-      </span>
-    </div>
+    <span className={`text-sm truncate px-3 ${isReady ? 'text-emerald-600 dark:text-emerald-400' : 'text-red-600 dark:text-red-400'}`}>
+      {cellValue}
+    </span>
   );
 });
 
